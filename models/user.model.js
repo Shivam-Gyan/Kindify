80e4b2f85098db4f4dbcdf96f06d13b858b9b6fe
Shivam Gupta <shivamgupta191092005@gmail.com>
2025-05-17 13:51:13 +0530

    Add NGO, User, and related schemas; update package.json for module support

---
+++ models/user.model.js
@@ -0,0 +1,61 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+    },
+    isEmailVerified: {
+        type: Boolean,
+        default: false,
+    },
+    isActive: {
+        type: Boolean,
+        default: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    phone: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        minlength: 10,
+        maxlength: 15,
+    },
+    address: {
+        type: String,
+        trim: true,
+        minlength: 5,
+        maxlength: 200,
+    },
+    role: {
+        type: String,
+        enum: ["Donor","NGO","Admin"],
+        default: "user",
+    },
+
+    // if role is NGO, then this field will be used for find the person designsation in NGO who is resgistering.
+    // only for NGO role not for donor or admin
+    designation: {
+        type: String,
+        trim: true,
+        enum: ["Manager", "Volunteer", "Coordinator", "Other"],
+        default: "Other",
+    },  
+
+},{timestamps: true});
+
+
+const User = mongoose.model("User", userSchema);
+export default User;
